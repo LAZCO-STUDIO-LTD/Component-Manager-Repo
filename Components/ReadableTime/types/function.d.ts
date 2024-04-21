019ec8372d6d6d66842f8b363baefbb68679cc5f
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-22 02:40:01 +0800

    feat: ✨ create "ReadableTime" component

---
+++ Components/ReadableTime/types/function.d.ts
@@ -0,0 +1,14 @@
+export type milliseconds = number;
+
+type Return = {
+  specific: {
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    minute: number;
+    second: number;
+  };
+  string: string;
+};
+export default Return;
